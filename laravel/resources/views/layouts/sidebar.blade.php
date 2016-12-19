2ed0cfeafca3be5503a82160984a8f59341fc15c
imwota <isallzack16@gmail.com>
2016-12-19 10:26:31 +0300

    AdminLTE Template and deleted the inventory directory

--- laravel/resources/views/layouts/sidebar.blade.php
+++ laravel/resources/views/layouts/sidebar.blade.php
@@ -1,28 +1,46 @@
-<!doctype html>
-        <html>
-<head>
-    @include('includes.head')
+<!-- Left side column. contains the sidebar -->
+<aside class="main-sidebar">
 
-</head>
-<body>
-<div class="container">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
 
-    <header class="row">
-        @include('includes.header')
-    </header>
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel">
+            <div class="pull-left image">
+                <img src="{{ asset("/bower_components/AdminLTE/dist/img/user2-160x160.jpg") }}" class="img-circle" alt="User Image" />
+            </div>
+            <div class="pull-left info">
+                <p>Alexander Pierce</p>
+                <!-- Status -->
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
 
-    <div id="main" class="row">
-        <!--sidebar content -->
-    <div id="content" class="col-md-4">
-        @include('includes.sidebar')
-    </div>
-    <div id="content" class="col-md-8">
-        @yield('content')
-    </div>
-    </div>
-    <footer class=""row">
-    @include('includes.footer')
-    </footer>
-</div>
-</body>
-</html>
+        <!-- search form (Optional) -->
+        <form action="#" method="get" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="q" class="form-control" placeholder="Search..."/>
+                <span class="input-group-btn">
+  <button type='submit' name='search' id='search-btn' class="btn btn-flat"><i class="fa fa-search"></i></button>
+</span>
+            </div>
+        </form>
+        <!-- /.search form -->
+
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
+            <li class="header">HEADER</li>
+            <!-- Optionally, you can add icons to the links -->
+            <li class="active"><a href="#"><span>Link</span></a></li>
+            <li><a href="#"><span>Another Link</span></a></li>
+            <li class="treeview">
+                <a href="#"><span>Multilevel</span> <i class="fa fa-angle-left pull-right"></i></a>
+                <ul class="treeview-menu">
+                    <li><a href="#">Link in level 2</a></li>
+                    <li><a href="#">Link in level 2</a></li>
+                </ul>
+            </li>
+        </ul><!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+</aside>
