54b595204a762003cf2a64f0d2ce9f3eef50dc73
dkusiima <shirleydaizy@gmail.com>
2017-01-12 12:44:17 +0300

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+
+@section('content')
+    <section class="row new-post">
+        <div class="col-md-6 col-md-offset-3">
+            <header><h3> Welcome To TODS </h3> </header>
+            <form action>
+
+            </form>
+        </div>
+
+    </section>
+@endsection
