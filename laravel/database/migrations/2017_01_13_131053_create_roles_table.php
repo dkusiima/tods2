32e965c4dfad09999663cd08b9c64f2f2eb62028
imwota <isallzack16@gmail.com>
2017-01-16 11:57:44 +0300

    Added the roles migration

---
+++ laravel/database/migrations/2017_01_13_131053_create_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('roles', function (Blueprint $table) {
+        $table->increments('role_id');
+        $table->string('name');
+        $table->string('status');
+        $table->string('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
